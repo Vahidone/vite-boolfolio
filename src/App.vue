439287538d2d18872dabaebc36e1551fd6417f91
Vahidone <v.ban.roma@gmail.com>
2023-12-06 16:55:39 +0100

    init: add readme and make styles

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+
+import axios from 'axios';
+import { store } from './data/store';
+
+
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+
+      title: 'Hello World',
+
+    }
+  },
+
+  methods: {
+    
+    getApi() {
+      axios.get(store.apiUrl + 'projects')
+
+      .then(results => {
+          console.log(results.data);
+        })
+    }
+
+  }
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
