a0e9ccf0c501306535f59d2c90f995cf4894fc99
Vahidone <v.ban.roma@gmail.com>
2023-12-07 16:28:51 +0100

    feat: create Home component

---
+++ src/pages/Home.vue
@@ -0,0 +1,22 @@
+
+<script>
+  export default {
+    name: 'Home',
+    data() {
+      return {}
+    },
+    computed: {},
+    mounted() {},
+    methods: {}
+  }
+</script>
+
+
+<template>
+  <div>Home</div>
+</template>
+
+
+<style lang="scss">
+
+</style>
